a6c5db89acfb03c05341eefedfaf8b3dfd46a1e8
Wawa0613 <xianglee07269@gmail.com>
2025-07-16 17:39:23 -0700

    chore: minor adjustments and refinements

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.payload.CartItemDTO;
 import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.service.CartService;
 import com.ecommerce.project.service.CartServiceImpl;
@@ -24,11 +25,19 @@ public class CartController {
     CartRepository cartRepository;
     @Autowired
     private AuthUtil authUtil;
+    @PostMapping("/carts/create")
+    public ResponseEntity<String> createOrUpdateCart(@RequestBody List<CartItemDTO> cartItems){
+        String response =cartService.createOrUpdateCart(cartItems);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
-    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId, @PathVariable Integer quantity){
-        CartDTO cartDTO=cartService.addProductToCart(productId,quantity);
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
     }
+
 
     @GetMapping("/carts")
     public ResponseEntity<List<CartDTO>>getACarts(){
